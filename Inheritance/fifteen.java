3cf8f2e753b9066a3471068214a504c2cd7eac5d
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-20 23:50:05 +0530

    added abstract questions

---
+++ Inheritance/fifteen.java
@@ -0,0 +1,29 @@
+
+abstract class Animal {
+
+void sound();
+
+}
+
+class Lion extends Animal {
+
+void sound (){
+    System.out.println("Lion roars");   
+}
+
+}
+
+class Tiger extends Animal{
+void sound (){
+    System.out.println("Tiger roars");   
+}
+
+}
+
+class main {
+
+public static void main(String st[]){
+    
+}
+
+}
